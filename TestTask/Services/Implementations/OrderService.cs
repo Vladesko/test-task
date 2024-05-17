69f849590d109d5ee18773123e781abc81025427
Vladesko <borodich01@list.ru>
2024-05-17 16:10:07 +0300

    Add

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext context;
+        public OrderService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Order> GetOrder()
+        {
+            var order = await context.Orders.
+                AsNoTracking().
+                Where(o => o.Quantity > 1).
+                OrderByDescending(o => o.CreatedAt).
+                FirstOrDefaultAsync();
+
+
+            if (order == null)
+                throw new Exception("Order is not found");
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = await context.Orders
+                 .AsNoTracking()
+                 .Where(o => o.User.Status == UserStatus.Active)
+                 .OrderByDescending(o => o.CreatedAt)
+                 .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext context;
+        public UserService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var result = await context.Users
+                .AsNoTracking()
+                .Where(u => u.Orders.Any(o => o.Status == OrderStatus.Delivered))
+                .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2003))
+                .Select(u => new
+                {
+                    User = u,
+                    TotalSum = u.Orders.Where(o => o.Status == OrderStatus.Delivered)
+                .Where(o => o.UserId == u.Id)
+                 .Sum(o => o.Price)
+                })
+                  .OrderByDescending(u => u.TotalSum)
+                .FirstAsync();
+
+
+
+            if (result == null)
+                throw new Exception("User is not found");
+
+            return result.User;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = await context.Users.AsNoTracking().
+                Where(u => u.Orders.Any(o => o.Status == OrderStatus.Paid)).
+                Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2010)).
+                ToListAsync();
+
+            return users;
+        }
+    }
+}
